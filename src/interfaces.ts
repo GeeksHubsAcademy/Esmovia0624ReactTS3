4add6a5da2aabe12fd838b8779e6bbcd7c323cd6
Dave86dev <davidochando1986@gmail.com>
2024-07-10 09:58:54 +0200

    feat: basic TS api call and deep typing

---
+++ src/interfaces.ts
@@ -0,0 +1,14 @@
+
+export interface Answer  {
+    success: boolean,
+    data: FetchTodo[],
+    message: string
+}
+
+export interface FetchTodo {
+    userId: number,
+    id: number,
+    title: "string",
+    completed: boolean
+}
+
